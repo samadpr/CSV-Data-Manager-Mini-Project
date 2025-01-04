648b99b3f8c94daf8f1191c5b3e52511bf4e9b71
Abdul Samad <codinguse341@gmail.com>
2025-01-04 15:50:44 +0530

    Project Added

---
+++ CsvDataManager/CsvDataManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CsvDataManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CsvDataManager/QueueWorkerConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
